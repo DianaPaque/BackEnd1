4cd2245beeda7586c32a0ecf0da79f110192f38a
DianaPaque <dianas.paquep@autonoma.edu.co>
2025-01-23 17:55:19 -0500

    first commit

---
+++ Ejemplo1/src/main/java/com/mycompany/ejemplo1/Ejemplo1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejemplo1 {
+    private String serverAddress;
+    private int port; 
+    private Socket clientSocked;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    
+    
+}
---
+++ Ejemplo1/src/main/java/networkLayer/networkClient.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package networkLayer;
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class networkClient {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = in.nextLine();
+        System.out.println("Type your last name: ");
+        String lastName = in.nextLine();
+        System.out.println("Name: " + name + " Last Name: " + lastName);
+    }
+}
